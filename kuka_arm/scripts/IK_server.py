90f1b092a9cc12285519998e086971bd6caedcdd
Unknown <ulrichludmann@gmail.com>
2017-09-27 11:02:53 -0700

    Revert "feat: implement solver for inverse orientation"
    This reverts commit 65d548490720f1fc4da161eacdf097b40e209609.

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -71,6 +71,61 @@ def rot_z(q):
             [0, 0, 0, 1]])
     return r_z
 
+<<<<<<< HEAD
+=======
+def extract_rotation_matrix(rotation_matrix):
+    return rotation_matrix[0:3, 0:3]
+
+
+
+
+class Robot():
+    def __init__(self, dh_params):
+        self.dh_params = dh_params
+        self.forward_kinematics(dh_params)
+        self.dh_params = dh_params
+        self.R0_3 = self.T_01[0:3, 0:3] * self.T_12[0:3, 0:3] * self.T_23[0:3, 0:3]
+
+    def get_thetas(self, x, y, z):
+        #theta 1
+        q1 = atan2(self.y, self.x)
+        # project to x-z layer:
+        y = y / sin(q1)
+        x = x / cos(q1)
+        # joint offset substraction
+        x -= 0.35
+        z -= 0.75
+        # static lengths 
+        l_a = 1.50 #length link 4(0.96) + length_link5 (0.54)
+        l_c = 1.25
+        l_b = sqrt(x ** 2 + z ** 2)
+        beta = acos((l_a**2 + l_c**2 - l_b**2) / (2 * l_a * l_c))
+        q3 = 90 * dtr - beta
+        h = atan2(z, x)
+        alpha = acos((l_c**2 + l_b**2 - l_a**2) / (2 * l_c * l_b))
+        q2 = 90 *dtr - h - alpha
+        return q1.evalf(), q2.evalf(), q3.evalf()
+
+    def forward_kinematics(self, dh_params):
+        self.T_01 = dh_transformation_step(alpha0, a0, d1, q1).subs(self.dh_params)
+        self.T_12 = dh_transformation_step(alpha1, a1, d2, q2).subs(self.dh_params)
+        self.T_23 = dh_transformation_step(alpha2, a2, d3, q3).subs(self.dh_params)
+        self.T_34 = dh_transformation_step(alpha3, a3, d4, q4).subs(self.dh_params)
+        self.T_45 = dh_transformation_step(alpha4, a4, d5, q5).subs(self.dh_params)
+        self.T_56 = dh_transformation_step(alpha5, a5, d6, q6).subs(self.dh_params)
+        self.T_6G = dh_transformation_step(alpha6, a6, d7, q7).subs(self.dh_params)
+        self.R_corr = rot_z(180 * dtr) * rot_y(-90 * dtr)
+        self.T_0G = self.T_01 * self.T_12 * self.T_23 * self.T_34 * self.T_45 * self.T_56 * self.T_6G * self.R_corr
+
+    def inverse_kinematics(self, x, y, z, roll, pitch, yaw):
+        self.R_EE = rot_z(roll) * rot_y(pitch) * rot_x(yaw) * R_corr #rotation of the end effector
+
+
+KR210 = Robot(dh_params)
+
+
+
+>>>>>>> parent of 65d5484... feat: implement solver for inverse orientation
 def get_thetas(x, y, z):
     #theta 1
     q1 = atan2(y, x)
@@ -183,6 +238,7 @@ def handle_calculate_IK(req):
             wy = py - (dh_params[d6] + l_ee) * ny
             wz = pz - (dh_params[d6] + l_ee) * nz
 
+<<<<<<< HEAD
             theta1, theta2, theta3 = get_thetas(wx, wy, wz)
 
             ###inverse orientation problem
@@ -196,6 +252,23 @@ def handle_calculate_IK(req):
             theta5 = atan2(sqrt(R3_6[0, 2] * R3_6[0,2] + R3_6[2,2] * R3_6[2,2]), R3_6[1,2])
             theta6 = atan2(-R3_6[1,1], R3_6[1,0])
 		
+=======
+            # Calculate joint angles using Geometric IK method
+            theta1, theta2, theta3 = get_thetas(wx, wy, wz)
+    	    #
+    	    ###inverse orientation problem
+
+            #R0_3 = T_01[0:3, 0:3] * T_12[0:3, 0:3] * T_23[0:3, 0:3]
+            R0_3 = KR210.R0_3.evalf(subs ={q1: theta1, q2: theta2, q3: theta3})
+            R3_6 = R0_3.inv('LU') * extract_rotation_matrix(R_EE)
+    	    #
+    	    #
+            ### Euler angles implementation from solution
+            #theta4 = atan2(R3_6[0,2], R3_6[2,2])
+            theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+            theta5 = atan2(sqrt(R3_6[0, 2] * R3_6[0, 2]+ R3_6[2, 2]*R3_6[2, 2]), R3_6[1, 2])
+            theta6 = atan2(-R3_6[1, 1], R3_6[1, 0])
+>>>>>>> parent of 65d5484... feat: implement solver for inverse orientation
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
             joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
